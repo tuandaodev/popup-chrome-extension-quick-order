5a3d8883b28aada54d05d7861041f4e883ca5fac
tuandaodev <tuandao.dev@gmail.com>
2018-12-03 19:19:35 +0700

    up

--- background.js
+++ background.js
@@ -2,5 +2,24 @@ console.log('TEST BACK');
 
 chrome.commands.onCommand.addListener(function(command) {
     console.log('Command:', command);
+    if(command === "AHoTen"){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            console.log(tabs[0].id);
+            chrome.tabs.sendMessage(tabs[0].id, { action: "HoTen" });
+        });
+    }
+    
+    if(command === "BDienThoai"){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            console.log(tabs[0].id);
+            chrome.tabs.sendMessage(tabs[0].id, { action: "DienThoai" });
+        });
+    }
+    
+    if(command === "CDiaChi"){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            console.log(tabs[0].id);
+            chrome.tabs.sendMessage(tabs[0].id, { action: "DiaChi" });
+        });
+    }
 });
-
--- key_event.js
+++ key_event.js
@@ -1,70 +1,151 @@
-if (window == top) {
-    window.addEventListener('keyup', doKeyPress, false); //add the keyboard handler
+//if (window == top) {
+//    window.addEventListener('keyup', doKeyPress, false); //add the keyboard handler
+//}
+
+chrome.runtime.onMessage.addListener(request => {
+  console.log(request);
+  if (request.action === "HoTen") {
+      HoTen();
+  };
+  
+  if (request.action === "DienThoai") {
+      DienThoai();
+  };
+  
+  if (request.action === "DiaChi") {
+      DiaChi();
+  };
+});
+
+
+function HoTen() {
+    console.log("Get họ tên");
+    var current_text = window.getSelection().toString();
+    if (!current_text || current_text == "") {
+        console.log("Get clipboard");
+        navigator.clipboard.readText()
+            .then(text => {
+//              console.log('Pasted content: ', text);
+                current_text = text;
+                chrome.extension.sendRequest({storage: 'client_name', value: current_text});
+            })
+            .catch(err => {
+              console.log('Failed to read clipboard contents: ', err);
+            });
+    } else {
+        console.log(current_text);
+        chrome.extension.sendRequest({storage: 'client_name', value: current_text});
+    }
+    chrome.extension.sendRequest({storage: 'client_url', value: window.location.href});
 }
+
+function DienThoai() {
+    console.log("Get điện thoại");
+    var current_text = window.getSelection().toString();
+    if (!current_text || current_text == "") {
+        console.log("Get clipboard");
+        navigator.clipboard.readText()
+            .then(text => {
+//              console.log('Pasted content: ', text);
+                current_text = text;
+                chrome.extension.sendRequest({storage: 'client_phone', value: current_text});
+            })
+            .catch(err => {
+              console.log('Failed to read clipboard contents: ', err);
+            });
+    } else {
+        console.log(current_text);
+        chrome.extension.sendRequest({storage: 'client_phone', value: current_text});
+    }
+    chrome.extension.sendRequest({storage: 'client_url', value: window.location.href});
+}
+
+function DiaChi() {
+    console.log("Get địa chỉ");
+    var current_text = window.getSelection().toString();
+
+    if (!current_text || current_text == "") {
+        console.log("Get clipboard");
+        navigator.clipboard.readText()
+            .then(text => {
+//              console.log('Pasted content: ', text);
+                current_text = text;
+                chrome.extension.sendRequest({storage: 'client_address', value: current_text});
+            })
+            .catch(err => {
+              console.log('Failed to read clipboard contents: ', err);
+            });
+    } else {
+        console.log(current_text);
+        chrome.extension.sendRequest({storage: 'client_address', value: current_text});
+    }
+    chrome.extension.sendRequest({storage: 'client_url', value: window.location.href});
+}
+
 
 //KeyCode: 49 => Num1 ...
-function doKeyPress(e){
-	if (e.shiftKey && e.keyCode == 50){ 
-		console.log("Shift + 2: Get họ tên");
-                var current_text = window.getSelection().toString();
-                if (!current_text || current_text == "") {
-                    console.log("Get clipboard");
-                    navigator.clipboard.readText()
-                        .then(text => {
-                          console.log('Pasted content: ', text);
-                            current_text = text;
-                            chrome.extension.sendRequest({storage: 'client_name', value: current_text});
-                        })
-                        .catch(err => {
-                          console.log('Failed to read clipboard contents: ', err);
-                        });
-                } else {
-                    console.log(current_text);
-                    chrome.extension.sendRequest({storage: 'client_name', value: current_text});
-                }
-                chrome.extension.sendRequest({storage: 'client_url', value: window.location.href});
-	}
-        
-        if (e.shiftKey && e.keyCode == 51){
-		console.log("Shift + 3: Get điện thoại");
-                var current_text = window.getSelection().toString();
-                if (!current_text || current_text == "") {
-                    console.log("Get clipboard");
-                    navigator.clipboard.readText()
-                        .then(text => {
-                          console.log('Pasted content: ', text);
-                            current_text = text;
-                            chrome.extension.sendRequest({storage: 'client_phone', value: current_text});
-                        })
-                        .catch(err => {
-                          console.log('Failed to read clipboard contents: ', err);
-                        });
-                } else {
-                    console.log(current_text);
-                    chrome.extension.sendRequest({storage: 'client_phone', value: current_text});
-                }
-                chrome.extension.sendRequest({storage: 'client_url', value: window.location.href});
-	}
-        
-        if (e.shiftKey && e.keyCode == 52){
-            console.log("Shift + 4: Get địa chỉ");
-            var current_text = window.getSelection().toString();
-
-            if (!current_text || current_text == "") {
-                console.log("Get clipboard");
-                navigator.clipboard.readText()
-                    .then(text => {
-                      console.log('Pasted content: ', text);
-                        current_text = text;
-                        chrome.extension.sendRequest({storage: 'client_address', value: current_text});
-                    })
-                    .catch(err => {
-                      console.log('Failed to read clipboard contents: ', err);
-                    });
-            } else {
-                console.log(current_text);
-                chrome.extension.sendRequest({storage: 'client_address', value: current_text});
-            }
-            chrome.extension.sendRequest({storage: 'client_url', value: window.location.href});
-	}
-}
+//function doKeyPress(e){
+//	if (e.shiftKey && e.keyCode == 50){
+//		console.log("Shift + 2: Get họ tên");
+//                var current_text = window.getSelection().toString();
+//                if (!current_text || current_text == "") {
+//                    console.log("Get clipboard");
+//                    navigator.clipboard.readText()
+//                        .then(text => {
+//                          console.log('Pasted content: ', text);
+//                            current_text = text;
+//                            chrome.extension.sendRequest({storage: 'client_name', value: current_text});
+//                        })
+//                        .catch(err => {
+//                          console.log('Failed to read clipboard contents: ', err);
+//                        });
+//                } else {
+//                    console.log(current_text);
+//                    chrome.extension.sendRequest({storage: 'client_name', value: current_text});
+//                }
+//                chrome.extension.sendRequest({storage: 'client_url', value: window.location.href});
+//	}
+//        
+//        if (e.shiftKey && e.keyCode == 51){
+//		console.log("Shift + 3: Get điện thoại");
+//                var current_text = window.getSelection().toString();
+//                if (!current_text || current_text == "") {
+//                    console.log("Get clipboard");
+//                    navigator.clipboard.readText()
+//                        .then(text => {
+//                          console.log('Pasted content: ', text);
+//                            current_text = text;
+//                            chrome.extension.sendRequest({storage: 'client_phone', value: current_text});
+//                        })
+//                        .catch(err => {
+//                          console.log('Failed to read clipboard contents: ', err);
+//                        });
+//                } else {
+//                    console.log(current_text);
+//                    chrome.extension.sendRequest({storage: 'client_phone', value: current_text});
+//                }
+//                chrome.extension.sendRequest({storage: 'client_url', value: window.location.href});
+//	}
+//        
+//        if (e.shiftKey && e.keyCode == 52){
+//            console.log("Shift + 4: Get địa chỉ");
+//            var current_text = window.getSelection().toString();
+//
+//            if (!current_text || current_text == "") {
+//                console.log("Get clipboard");
+//                navigator.clipboard.readText()
+//                    .then(text => {
+//                      console.log('Pasted content: ', text);
+//                        current_text = text;
+//                        chrome.extension.sendRequest({storage: 'client_address', value: current_text});
+//                    })
+//                    .catch(err => {
+//                      console.log('Failed to read clipboard contents: ', err);
+//                    });
+//            } else {
+//                console.log(current_text);
+//                chrome.extension.sendRequest({storage: 'client_address', value: current_text});
+//            }
+//            chrome.extension.sendRequest({storage: 'client_url', value: window.location.href});
+//	}
+//}
