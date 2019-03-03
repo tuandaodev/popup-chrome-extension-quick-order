2e90aecdb1de3da385624c301e8b1dac74306d6b
tuandaodev <tuandao.dev@gmail.com>
2019-03-03 17:18:04 +0700

    up

--- js/home.js
+++ js/home.js
@@ -7,6 +7,12 @@ if (employee_token) {
     var left = screen.width - w;
     var top = (screen.height/2)-(h/2); 
     
+    w = Math.round(w);
+    top_bar = Math.round(top_bar);
+    h = Math.round(h);
+    left = Math.round(left);
+    top = Math.round(top);
+    
     chrome.windows.create({url: "popup.html", type: "popup", height: h, width: w, left: left, top: top_bar});
     window.close();
 } else {
@@ -15,6 +21,11 @@ if (employee_token) {
     var h = 420;
     var left = (screen.width/2)-(w/2); 
     var top_bar = (screen.height/2)-(h/2); 
+    
+    w = Math.round(w);
+    h = Math.round(h);
+    left = Math.round(left);
+    top_bar = Math.round(top_bar);
     
     chrome.windows.create({url: "login.html", type: "popup", height: h, width: w, left: left, top: top_bar});
     window.close();
